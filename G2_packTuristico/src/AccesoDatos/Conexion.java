15de22ad2ec2a0bcaece220eb584080163f0ba70
dynamitesnake <ezeherrera77@gmail.com>
2024-10-20 23:25:35 -0300

    base de datos, acceso a datos (conexion)

---
+++ G2_packTuristico/src/AccesoDatos/Conexion.java
@@ -0,0 +1,32 @@
+
+package AccesoDatos;
+
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+public class Conexion {
+    private static final String URL="jdbc:mariadb://localhost:3306/";
+    private static final String DB="turismo";
+    private static final String USUARIO="root";
+    private static final String PASSWORD="";
+    private static Connection connection;
+    
+    private Conexion() {
+    }
+
+    public static Connection getConexion(){
+        
+        if(connection==null){
+            try {
+                Class.forName("org.mariadb.jdbc.Driver");
+                connection= DriverManager.getConnection(URL + DB, USUARIO,PASSWORD);
+                 JOptionPane.showMessageDialog(null,"Conexion exitosa");
+            } catch (ClassNotFoundException ex) {
+                JOptionPane.showMessageDialog(null, "Error a cargar driver");
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null,"Error a conectarse a la base de datos");
+            }
+       }
+     return connection;}    
+    
+}
---
+++ G2_packTuristico/src/g2_packturistico/G2_packTuristico.java
@@ -0,0 +1,15 @@
+
+package g2_packturistico;
+
+import AccesoDatos.Conexion;
+import java.sql.Connection;
+
+
+public class G2_packTuristico {
+
+    
+    public static void main(String[] args) {
+       Connection con = Conexion.getConexion();
+    }
+    
+}
