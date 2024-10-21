aedaed420582a6a7f2a20059503b13155a84bd50
dynamitesnake <ezeherrera77@gmail.com>
2024-10-21 08:45:40 -0300

    package

---
+++ G2_packTuristico/src/Vista/G2_packTuristico.java
@@ -0,0 +1,15 @@
+
+package Vista;
+
+import Modelo.Conexion;
+import java.sql.Connection;
+
+
+public class G2_packTuristico {
+
+    
+    public static void main(String[] args) {
+       Connection con = Conexion.getConexion();
+    }
+    
+}
